9412f736bae964bec1281fdf8015888c0fec9dfb
ransomjam99 <ransomjam99@gmail.com>
2026-01-24 15:56:02 +0100

    from codex

--- pattern-formatter/frontend/sw.js
+++ pattern-formatter/frontend/sw.js
@@ -1,25 +1,44 @@
-const CACHE_NAME = 'afrodocs-static-v2';
+const CACHE_NAME = 'afrodocs-static-v7';
 const ASSETS = [
   './',
   './?source=pwa',
   './index.html',
   './manifest.webmanifest',
-  './logo/logo.png'
+  './logo/logo.png',
+  './logo/logo-48x48.png',
+  './logo/logo-72x72.png',
+  './logo/logo-96x96.png',
+  './logo/logo-128x128.png',
+  './logo/logo-144x144.png',
+  './logo/logo-152x152.png',
+  './logo/logo-192x192.png',
+  './logo/logo-384x384.png',
+  './logo/logo-512x512.png'
 ];
+
+// Enable navigation preload if supported
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      // Delete old caches
+      const keys = await caches.keys();
+      await Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)));
+      
+      // Enable navigation preload if supported
+      if ('navigationPreload' in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+      
+      await self.clients.claim();
+    })()
+  );
+});
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(ASSETS))
       .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    ).then(() => self.clients.claim())
   );
 });
 
